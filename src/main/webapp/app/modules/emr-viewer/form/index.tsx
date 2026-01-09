486933b59955df93249338d85f37cb427b6d170a
clang-engineer <clang.engineer@gmail.com>
2026-01-09 22:21:02 +0900

    feat(emr-viewer): add EmrForm and update layout

---
+++ src/main/webapp/app/modules/emr-viewer/form/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const EmrForm = () => {
+  return <div className="flex h-screen">EmrForm</div>;
+};
+export default EmrForm;
--- src/main/webapp/app/modules/emr-viewer/page.tsx
+++ src/main/webapp/app/modules/emr-viewer/page.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import EmrTitle from './emr-title';
+import EmrForm from './form';
 
 const EmrPage = () => {
   return (
-    <div>
-      <h1>EMR Viewer</h1>
-      {/* EMR Viewer content goes here */}
+    <div className="flex h-screen">
+      <div className="w-2/7 bg-gray-200">
+        <div className="h-1/5 border-b border-gray-300">patient form</div>
+        <div className="h-2/5 p-4 overflow-y-auto">chart list</div>
+        <div className="h-2/5 p-4 overflow-y-auto">form list</div>
+      </div>
+      <div className="w-5/7 bg-gray-400">Right</div>
     </div>
   );
 };
-
 export default EmrPage;
